0cb61bb12a2c95c13172348cc95a1a3e27681d1f
lazycece <lazycece@gmail.com>
2026-09-06 23:23:37 +0800

    inti p

---
+++ src/test/java/com/lazycece/agentscope/AgentscopeJavaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazycece.agentscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentscopeJavaLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
